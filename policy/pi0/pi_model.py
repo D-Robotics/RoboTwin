7136b1cc4742c7aab5ad3bdc79330ad00704ed4b
weiyang.hu <weiyang.hu@d-robotics.cc>
2025-10-31 14:58:23 +0800

    undate task_config

--- policy/pi0/pi_model.py
+++ policy/pi0/pi_model.py
@@ -35,7 +35,7 @@ class PI0:
         self.policy = _policy_config.create_trained_policy(
             config,
             # f"policy/pi0/checkpoints/{self.train_config_name}/{self.model_name}/{self.checkpoint_id}",
-            f"/mnt/data/weiyang.hu/models/openpi/pi0_aloha_pytorch_hammer_demo_clean_1027/",
+            f"/mnt/data/weiyang.hu/models/openpi/pi0_aloha_pytorch_hammer_demo_clean_1031/",
             robotwin_repo_id=model_name)
         print("loading model success!")
         self.img_size = (224, 224)
